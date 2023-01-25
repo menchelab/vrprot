f73302f025b553f11e695847a8a6970eb1a26685
ObT1337 <a12048115@unet.univie.ac.at>
2023-01-26 00:16:22 +0100

    Added one_file_exe again

---
+++ one_file_executable/alphafold_vrnetzer.py
@@ -0,0 +1,36 @@
+#! python3
+import sys
+
+sys.path.append("/Users/till/Documents/Playground/alphafold_to_vrnetzer/pypi_project/src")
+import vrprot.util
+from vrprot.alphafold_db_parser import AlphafoldDBParser
+from vrprot.argument_parser import argument_parser
+from vrprot.util import Logger
+
+log = Logger("main")
+
+def main():
+    """Main function will take the arguments passed by the user and execute the program accordingly."""
+    args = argument_parser().parse_args()
+    parser = AlphafoldDBParser()
+    if args.mode == "clear":
+        parser.clear_default_dirs()
+        exit()
+    parser.set_batch_size(args)
+    parser.set_dirs(args)
+    parser.set_alphafold_version(args)
+    parser.set_coloring_mode(args)
+    parser.set_chimerax(args)
+    log.info(f"AlphaFold Version DB: {parser.alphafold_ver}")
+    if args.mode == "fetch":
+        parser.execute_fetch(args.proteins[0])
+    if args.mode == "local":
+        parser.execute_local(args.source[0])
+    if args.mode == "list":
+        with open(args.file[0]) as f:
+            proteins = f.read().splitlines()
+        parser.execute_from_object(proteins)
+
+
+if __name__ == "__main__":
+    main()
